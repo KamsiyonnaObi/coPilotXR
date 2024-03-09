fdea825ebd3fa8898792cdeaa0db66fb2ee14da8
Kamsiyonna <86691010+KamsiyonnaObi@users.noreply.github.com>
2024-03-09 17:38:57 -0600

    chore: refactor main page layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,46 +1,18 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import type { Metadata } from "next";
-import localFont from "next/font/local";
-import "./globals.css";
 import { cn } from "../../shadcn/lib/utils";
 
+import localFont from "next/font/local";
+import { adelleFonts, neuzeitFonts } from "./constants";
+import "./globals.css";
+
 const adelle = localFont({
-  src: [
-    {
-      path: "../../public/fonts/Adelle/Adelle-Bold.otf",
-      weight: "700",
-    },
-    {
-      path: "../../public/fonts/Adelle/Adelle-Semibold.otf",
-      weight: "600",
-    },
-    {
-      path: "../../public/fonts/Adelle/Adelle-regular.otf",
-      weight: "400",
-    },
-  ],
+  src: adelleFonts,
   variable: "--font-adelle",
 });
 
 const neuzeit = localFont({
-  src: [
-    {
-      path: "../../public/fonts/Neuzeit/Neuzeit-Black.otf",
-      weight: "800",
-    },
-    {
-      path: "../../public/fonts/Neuzeit/Neuzeit-Bold.otf",
-      weight: "700",
-    },
-    {
-      path: "../../public/fonts/Neuzeit/Neuzeit-Regular.otf",
-      weight: "400",
-    },
-    {
-      path: "../../public/fonts/Neuzeit/Neuzeit-Light.otf",
-      weight: "300",
-    },
-  ],
+  src: neuzeitFonts,
   variable: "--font-neuzeit",
 });
 
