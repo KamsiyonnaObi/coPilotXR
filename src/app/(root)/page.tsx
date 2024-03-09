6f77db736af868aba57fe76afc2a0870f593a406
Kamsiyonna <86691010+KamsiyonnaObi@users.noreply.github.com>
2024-03-09 14:03:25 -0600

    chore: final layout changes

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,7 +1,6 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <section></section>
       <div>
         <h1 className="font-Adelle font-bold">This should be adelle bold</h1>
         <p className="font-Adelle font-semibold">
