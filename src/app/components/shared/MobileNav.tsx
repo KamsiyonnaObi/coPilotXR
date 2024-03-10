bb6f2419941b3efbf15889f1b50d8e5127b885c1
Kamsiyonna <86691010+KamsiyonnaObi@users.noreply.github.com>
2024-03-10 09:58:35 -0500

    mobile nav with sheet

---
+++ src/app/components/shared/MobileNav.tsx
@@ -0,0 +1,39 @@
+import { SignedIn, UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { Sheet, SheetContent, SheetTrigger } from "shadcn/components/ui/sheet";
+
+const MobileNav = () => {
+  return (
+    <header className="header">
+      <Link href="/" className="flex items-center gap-2 md:py-2">
+        <Image
+          src="/assets/images/CoPilotXR_02.png"
+          alt="logo"
+          width={180}
+          height={28}
+        />
+      </Link>
+      <nav className="flex gap-2">
+        <SignedIn>
+          <UserButton afterSignOutUrl="/" />
+          <Sheet>
+            <SheetTrigger>
+              <Image
+                src="/assets/icons/menu.svg"
+                alt="menu"
+                width={32}
+                height={32}
+                className="cursor-pointer"
+              />
+            </SheetTrigger>
+            <SheetContent></SheetContent>
+          </Sheet>
+        </SignedIn>
+      </nav>
+    </header>
+  );
+};
+
+export default MobileNav;
