c27ec2e9fa51c9439299459f6cf78b2d6655164e
Kamsiyonna <86691010+KamsiyonnaObi@users.noreply.github.com>
2024-03-12 10:08:58 -0500

    create media uploader component

--- src/app/(root)/transformations/add/[type]/page.tsx
+++ src/app/(root)/transformations/add/[type]/page.tsx
@@ -1,7 +1,18 @@
+import MediaUploader from "@/src/app/components/shared/MediaUploader";
 import React from "react";
 
 const AddTransformationTypePage = () => {
-  return <div>AddTransformationTypePage</div>;
+  return (
+    <div className="media-uploader-field">
+      <MediaUploader
+        onValueChange={() => {}}
+        setImage={() => {}}
+        publicId=""
+        image=""
+        type=""
+      />
+    </div>
+  );
 };
 
 export default AddTransformationTypePage;
---
+++ src/app/components/shared/MediaUploader.tsx
@@ -0,0 +1,8 @@
+import { useToast } from "@/shadcn/components/ui/use-toast";
+
+const MediaUploader = () => {
+  const { toast } = useToast();
+  return <div>MediaUploader</div>;
+};
+
+export default MediaUploader;
