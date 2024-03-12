14a7e39def2ad9a53e8c54a623599630e80abf6c
Kamsiyonna <86691010+KamsiyonnaObi@users.noreply.github.com>
2024-03-12 08:28:32 -0500

    expose all api

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/", "/src/app/api/webhooks/clerk"],
+  publicRoutes: ["/", "/api/*"],
 });
 
 export const config = {
