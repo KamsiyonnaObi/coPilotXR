eb60dd727d2944d1d4c7b707aa13eeca1666cc6a
Kamsiyonna <86691010+KamsiyonnaObi@users.noreply.github.com>
2024-03-13 08:28:54 -0500

    update links and routes to coming soon

---
+++ src/app/(root)/image-upload/page.tsx
@@ -0,0 +1,12 @@
+import MediaUploader from "@/src/app/components/shared/MediaUploader";
+import React from "react";
+
+const ImageUpload = () => {
+  return (
+    <div className="media-uploader-field">
+      <MediaUploader />
+    </div>
+  );
+};
+
+export default ImageUpload;
--- src/app/(root)/transformations/add/[type]/page.tsx
+++ src/app/(root)/transformations/add/[type]/page.tsx
@@ -1,17 +1,11 @@
-import MediaUploader from "@/src/app/components/shared/MediaUploader";
+import Header from "@/src/app/components/shared/Header";
 import React from "react";
 
 const AddTransformationTypePage = () => {
   return (
-    <div className="media-uploader-field">
-      <MediaUploader
-        onValueChange={() => {}}
-        setImage={() => {}}
-        publicId=""
-        image=""
-        type=""
-      />
-    </div>
+    <h1 className="">
+      <Header title="Coming Soon" subtitle="Features currently being built" />
+    </h1>
   );
 };
 
