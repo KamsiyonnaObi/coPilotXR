6087a2e33e9731a883026dd1c92305a44872c0ae
Kamsiyonna <86691010+KamsiyonnaObi@users.noreply.github.com>
2024-03-13 07:22:37 -0500

    feature homepage layout

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,8 +1,15 @@
+import Collection from "../components/shared/Collection";
+
 export default async function Home() {
+  const images = { data: ["1", "2", "3", "4"] }; // TODO change to back end call
   return (
     <>
       <section className="home">
         <h1 className="home-heading">Welcome To CoPilot Image Uploader</h1>
+      </section>
+      {/* Gallery */}
+      <section className="sm:mt-12">
+        <Collection images={images?.data} />
       </section>
     </>
   );
