315e8fe27f43b8be1a2bbb3e88f5de961e22d5fa
Kamsiyonna <86691010+KamsiyonnaObi@users.noreply.github.com>
2024-03-14 09:12:08 -0500

    feat: profile page layout

--- src/app/(root)/profile/page.tsx
+++ src/app/(root)/profile/page.tsx
@@ -1,8 +1,31 @@
 import React from "react";
 import Header from "../../components/shared/Header";
+import Divider from "../../components/shared/Divider";
 
-const ProfilePage = () => {
-  return  <Header title="Your Profile" />;
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { getUserById } from "../../lib/actions/user.action";
+import Account from "../../components/shared/profile-sections/Account";
+import BillingSection from "../../components/shared/profile-sections/Billing";
+
+const ProfilePage = async () => {
+  const { userId } = auth();
+
+  if (!userId) {
+    redirect("/sign-in");
+  }
+
+  const user = await getUserById(userId);
+
+  // console.log(user);
+
+  return (
+    <>
+      <Header title="Your Profile" />
+      <Account photo={user.photo} username={user.username} email={user.email} />
+      <BillingSection creditBalance={user.creditBalance} planId={user.planId} />
+    </>
+  );
 };
 
 export default ProfilePage;
