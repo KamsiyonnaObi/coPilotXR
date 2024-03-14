fadca1668894d311d76ce6b36537289bbdce2d3d
Kamsiyonna <86691010+KamsiyonnaObi@users.noreply.github.com>
2024-03-14 09:56:09 -0500

    feature: edit profile

--- src/app/(root)/profile/page.tsx
+++ src/app/(root)/profile/page.tsx
@@ -7,6 +7,7 @@ import { redirect } from "next/navigation";
 import { getUserById } from "../../lib/actions/user.action";
 import Account from "../../components/shared/profile-sections/Account";
 import BillingSection from "../../components/shared/profile-sections/Billing";
+import Link from "next/link";
 
 const ProfilePage = async () => {
   const { userId } = auth();
@@ -21,7 +22,16 @@ const ProfilePage = async () => {
 
   return (
     <>
-      <Header title="Your Profile" />
+      <section className="flex-between">
+        <Header title="Your Profile" />
+        <Link
+          className="bg-main-700 p-14-bold py-3 px-4 rounded-md text-white hover:bg-red-400 focus:ring-2 focus:ring-dark-700"
+          href={"/user-profile"}
+        >
+          {" "}
+          Edit Profile{" "}
+        </Link>
+      </section>
       <Account photo={user.photo} username={user.username} email={user.email} />
       <BillingSection creditBalance={user.creditBalance} planId={user.planId} />
     </>
---
+++ src/app/(root)/user-profile/[[...user-profile]]/page.tsx
@@ -0,0 +1,6 @@
+import { UserProfile } from "@clerk/nextjs";
+const UserProfilePage = () => (
+  <UserProfile path="/user-profile" routing="path" />
+);
+
+export default UserProfilePage;
